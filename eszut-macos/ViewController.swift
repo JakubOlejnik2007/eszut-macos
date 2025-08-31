a877a4ec1a5bd3fa80b307623d6c4eb20f69adf2
Jakub Olejnik <jacobole2000@gmail.com>
2025-08-31 15:24:50 +0200

    Initial Commit

---
+++ eszut-macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  eszut-macos
+//
+//  Created by Jakub Olejnik on 31/08/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
